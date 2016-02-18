7d04afaac63e5baa2b7830b532799353093e8e63
Richard Doe <richard.doe@rwdit.net>
2016-02-18 09:11:11 +0000

    Test CCMA URL responses

--- test/app_test.rb
+++ test/app_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'json'
 
 class AppTest < Minitest::Test
   include Rack::Test::Methods
@@ -11,5 +12,13 @@ class AppTest < Minitest::Test
     get '/'
     assert last_response.not_found?
     assert_equal 'Not Found', last_response.body
+  end
+
+  def test_ccma_url
+    get '/', url: 'http://www.ccma.cat/tv3/alacarta/programa/titol/video/955989/'
+    assert last_response.ok?
+    json = JSON.parse(last_response.body)
+    assert_equal 'video', json['type']
+    assert_match %r{<iframe src="http://www.ccma.cat/video/embed/955989/"}, json['html']
   end
 end
