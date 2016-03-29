b750e564227097bd3cedb263ad215f2ea5919e18
Richard Doe <richard.doe@rwdit.net>
2016-03-29 09:00:33 +0100

    Change CREM-CNRS iframe height to 250

--- lib/europeana/oembed/responder/cremcnrs.rb
+++ lib/europeana/oembed/responder/cremcnrs.rb
@@ -5,7 +5,7 @@ module Europeana
         def html
           uri = URI.parse(@url)
           id = uri.path.match(%r{/items/([^/]+)/})[1]
-          %Q(<iframe width="361" height="215" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" ) +
+          %Q(<iframe width="#{width}" height="#{height}" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" ) +
             %Q(src="http://archives.crem-cnrs.fr/archives/items/#{id}/player/346x130/"></iframe>)
         end
 
@@ -14,7 +14,7 @@ module Europeana
         end
 
         def height
-          215
+          250
         end
       end
     end
