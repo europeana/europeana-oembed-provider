574bf393209b0b16c385b15415ae42c3d5c1469e
Kiffin Gish <kiffin.gish@planet.nl>
2018-07-13 22:27:42 +0200

    EA-1219: Check rights_url with allowed license urls

--- lib/europeana/oembed/sources/europeana/data-item.rb
+++ lib/europeana/oembed/sources/europeana/data-item.rb
@@ -10,8 +10,43 @@ require 'rdf/vocab'
 
 Europeana::OEmbed.register do |source|
 
+  def valid_rights_url(url)
+    u = url.sub(%r{^https?://},'')
+    %w{
+      creativecommons.org/publicdomain/mark/1.0
+      creativecommons.org/publicdomain/zero/1.0
+      creativecommons.org/licenses/by/1.0
+      creativecommons.org/licenses/by-sa/1.0
+    }.each do |rights_url|
+      return true if u.start_with?(rights_url) 
+    end
+    false
+  end
+
   def get_id(url)
     URI.parse(url).path.match(%r{/item/([0-9]+/[^/]+)})[1]
+  end
+
+  def get_rights_url(graph, provider_aggregation)
+    # Get the URL of the image from “object.aggregations[1].isShownBy”, then look for the respective web resource
+    # with the following JSON path expression and apply the additional logic below:
+    # “object.aggregations[1].webResources[.about={IMAGE_URL}].webResourceEdmRights”
+    # If no value exists get the default from: “object.aggregations[1].edmRights
+    #
+    rights_image_url = graph.query(subject: provider_aggregation, predicate: RDF::Vocab::EDM.isShownBy).first.object.to_s
+    begin
+      web_resources = graph.query(subject: provider_aggregation, predicate: RDF::Vocab::EDM.WebResource)
+    rescue NoMethodError => e
+      puts "ERROR: web_resource => #{e.inspect}"
+    rescue Exception => e
+      puts "ERROR: web_resource => #{e.inspect}"
+    end
+    
+    web_resources.each { |web_resource| puts "web_resource='${web_resource.inspect}'" }
+
+    rights_url = graph.query(subject: provider_aggregation, predicate: RDF::Vocab::EDM.rights).first.object.to_s
+    
+    return valid_rights_url(rights_url) ? rights_url : 'INVALID'
   end
 
   def call_api(url)
@@ -34,26 +69,24 @@ Europeana::OEmbed.register do |source|
     author_name = graph.query(subject: provider_aggregation, predicate: RDF::Vocab::EDM.dataProvider).first.object.to_s
     author_url = graph.query(subject: provider_aggregation, predicate: RDF::Vocab::EDM.isShownAt).first.object.to_s
 
-    rights_default_url = graph.query(subject: provider_aggregation, predicate: RDF::Vocab::EDM.rights).first.object.to_s
-    rights_image_url = graph.query(subject: provider_aggregation, predicate: RDF::Vocab::EDM.isShownBy).first.object.to_s
-    rights_url = rights_default_url
+    rights_url = get_rights_url(graph, provider_aggregation)
 
     provider_url = "#{ENV['API_PORTAL']}/#{id}.html"
 
     return {
-      title: title,
-      description: description,
-      author_name: author_name,
-      author_url: author_url,
-      provider_url: provider_url,
-      rights_url: rights_url
+        title: title,
+        description: description,
+        author_name: author_name,
+        author_url: author_url,
+        provider_url: provider_url,
+        rights_url: rights_url
     }
   end
 
   source.urls << %r{\Ahttp://data.europeana.eu/item/[0-9]+/[^/]+\z}
 
-  source.api = lambda { |url| call_api(url) }
-  source.id = lambda { |url| get_id(url) }
+  source.api = lambda {|url| call_api(url)}
+  source.id = lambda {|url| get_id(url)}
 
   source.respond_with do |response|
     response.type = :rich
