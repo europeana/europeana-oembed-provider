796e0badf1757455b4efc918c3be20360ad8a7f3
Kiffin Gish <kiffin.gish@planet.nl>
2018-07-21 10:36:04 +0200

    EA-1219: Created separate test suite for europeana api.

--- test/app_test.rb
+++ test/app_test.rb
@@ -57,14 +57,4 @@ class AppTest < Minitest::Test
     assert_equal 'video', json['type']
     assert_match %r{<iframe src="https://api.picturepipe.net/api/3.0/playouttoken/53728dac59db46c8a367663cd6359ddb/play}, json['html']
   end
-
-  # TODO
-  # def test_europeana_data_item_url
-  #   get '/', url: 'http://data.europeana.eu/item/9200397/BibliographicResource_3000126284212'
-  #   assert last_response.ok?
-  #   assert_equal 'application/json', last_response.headers['Content-Type']
-  #   json = JSON.parse(last_response.body)
-  #   assert_equal 'rich', json['type']
-  #   # assert_match %r{<iframe src="https://api.picturepipe.net/api/3.0/playouttoken/53728dac59db46c8a367663cd6359ddb/play}, json['html']
-  # end
 end
